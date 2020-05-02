c53fa5e448374026f9c2ea4b1fba7e0336c14c93
Fransiskusandika <dikakoko@icloud.com>
2020-05-02 16:52:09 +0700

    Updates

---
+++ precache-manifest.7018e74dac462a754021d11940fa14b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147aa8c3b053e8eb89fdb2adc708f8e1",
+    "url": "/calc-js/index.html"
+  },
+  {
+    "revision": "bc9d7fb204daad943b77",
+    "url": "/calc-js/static/css/main.d54edcc8.chunk.css"
+  },
+  {
+    "revision": "257e4ce563f0d571b912",
+    "url": "/calc-js/static/js/2.3b812025.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/calc-js/static/js/2.3b812025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9d7fb204daad943b77",
+    "url": "/calc-js/static/js/main.fd2cb6b7.chunk.js"
+  },
+  {
+    "revision": "534c7fc02fb41c1f0235",
+    "url": "/calc-js/static/js/runtime-main.e25ce2bd.js"
+  }
+]);
